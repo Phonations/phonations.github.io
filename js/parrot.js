f6414c4fb26940422d4fd25bc9595c611e20389c
Martin Delille <martin@phonations.com>
2016-03-27 17:02:40 +0200

    scrolling parrot

---
+++ js/parrot.js
@@ -0,0 +1,32 @@
+$(function() {
+  $(window).on('scroll', function() {
+    checkParrot();
+  });
+
+  checkParrot();
+
+  function checkParrot() {
+    var scrollTop = $(window).scrollTop();
+    var gap = document.body.scrollHeight -$(window).height() - 128;
+    var stickToTop = false;
+    var stickFixed = false;
+    var stickToBottom = false;
+    var parrot = $('#scrollingparrot');
+    var micstick = $('#scrollingmicstick');
+    console.log(scrollTop, gap, document.body.scrollHeight, $(window).height());
+    if(scrollTop < 15 + 59 + 138 + 2) {
+      stickToTop = true
+    } else if(scrollTop < gap) {
+      stickFixed = true;
+    } else {
+      stickToBottom = true;
+    }
+    parrot.toggleClass('parrotStickToTop', stickToTop);
+    parrot.toggleClass('parrotStickFixed', stickFixed);
+    parrot.toggleClass('parrotStickToBottom', stickToBottom);
+    micstick.toggleClass('micstickStickToTop', stickToTop);
+    micstick.toggleClass('micstickStickFixed', stickFixed);
+    micstick.toggleClass('micstickStickToBottom', stickToBottom);
+  };
+});
+
